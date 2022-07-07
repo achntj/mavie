46160e5c97fa5af68fe2248dd0b79c2a98c22135
achintyajha <achintyakjha@gmail.com>
2022-07-07 20:19:28 +0530

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,69 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
+import rehypePrism from "rehype-prism-plus";
+import FileSaver from "file-saver";
+import "prismjs/themes/prism-tomorrow.css";
 
 const Home: NextPage = () => {
+  const [text, setText] = useState("");
+  const [gfm, setGfm] = useState(true);
+  const [front, setFront] = useState(true);
+  function SaveFile() {
+    var blob = new Blob([text], {
+      type: "text/markdown;charset=utf-8",
+    });
+    FileSaver.saveAs(blob, "savedFile.md");
+  }
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="m-10 prose">
+      <h1 className="font-bold text-3xl mb-5">Notes</h1>
+      <div className="sm:flex justify-between items-center mb-2">
+        <button className="rounded p-1 bg-emerald-200" onClick={SaveFile}>
+          Export File
+        </button>
+        <div className="space-x-2">
+          <label>GFM</label>
+          <input
+            type="checkbox"
+            onClick={() => setGfm(!gfm)}
+            checked={gfm}
+          ></input>
+          <label>Frontmatter</label>
+          <input
+            type="checkbox"
+            onClick={() => setFront(!front)}
+            checked={front}
+          ></input>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </div>
+      <div className="sandbox flex border border-neutral-200">
+        <div className="w-1/2">
+          <textarea
+            className="resize-none outline-none box-border w-full h-full p-2"
+            placeholder="Start writing!"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+        </div>
+        <hr className="h-full border border-neutral-200 w-0" />
+        <div className="w-1/2 break-words overflow-y-scroll p-2">
+          <h2 className="text-neutral-400 mt-0">Preview</h2>
+          <hr className="!mb-5" />
+          <ReactMarkdown
+            children={text || "Start typing for instant preview! — ʕ•ᴥ•ʔ"}
+            remarkPlugins={[
+              ...(gfm ? [remarkGfm] : []),
+              ...(front ? [remarkFrontmatter] : []),
+            ]}
+            rehypePlugins={[rehypePrism]}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
